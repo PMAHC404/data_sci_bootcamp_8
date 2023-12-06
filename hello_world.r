e63c0077292b59f86699d8040fcbe805b92de34f
pornpipat61@gmail.com <pornpipat61@gmail.com>
2023-12-06 02:11:12 +0000

    update

--- hello_world.r
+++ hello_world.r
@@ -4,5 +4,10 @@ print("hello world")
 print("Oppenheimer is very good movie")
 
 library(dplyr)
-mtcars %>% select(1:5) %>%
-  filter(mpg > 30)
+mtcars %>% 
+  select(1:5) %>%
+    filter(mpg > 30)
+
+mtcars %>%
+  select(mpg) %>%
+    filter(mpp > 30)
